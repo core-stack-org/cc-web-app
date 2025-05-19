0d96572a4a16a59d4e1911c4706dfc32c4d23590
Ankit K. <ankit.kumar@oniondev.com>
2025-05-19 12:43:00 +0530

    ODK Public URLs update

--- src/helper/constants.js
+++ src/helper/constants.js
@@ -1,60 +1,74 @@
 exports.app_name = "commonsconnect";
 exports.app_domain = "https://nrm.core-stack.org";
 exports.GEOSERVER_URL = "https://geoserver.core-stack.org:8443";
-exports.api_url = "https://geoserver.core-stack.org/api/v1/";
-// exports.api_url = "https://8582-119-252-199-91.ngrok-free.app/api/v1/";
+// exports.api_url = "https://geoserver.core-stack.org/api/v1/";
+exports.api_url =
+  "https://9691-2001-df4-e000-3fc3-8e68-299f-3a9c-4e69.ngrok-free.app/api/v1/";
 exports.screen_mapping = { main: "nrega,admin", water: "delta_g" };
+// recharge structure
 exports.gw_odk_url =
-  "https://odk.gramvaani.org/-/single/SJfpkTOoBwx71amP1AUM1OGd8h72Ej4?st=DIovSA!ytbl6ixGi6jspvWUHFWj$kleMu$UIs3176kF$8xt2PGgIMSDp5hDIg6Db&d[/data/GPS_point_recharge_structure/point_mapsappearance]=";
+  "https://odk.core-stack.org/-/single/97dnXFfIiBSX8NflbKwiJeuo6jAkeji?st=dzcP1R16z3P$seU5QjKkgGjdav$pga66iH972V93scb8BxwvuBuQVxIUxLo$gf8F&d[/data/GPS_point_recharge_structure/point_mapsappearance]=";
 exports.wb_odk_url =
   "https://odk.gramvaani.org/-/single/IlhFTDvrF1Sib1NxpJrybXIfko9ZD80?st=sR!eg7pgy$DCm66Dw614v!PDHuFXFG61SyYo9mUvjb5l0IOTouwflpQvR0oxoJaX&d[/data/GPS_point/point_mapsappearance]=";
 exports.agri_irr_work_odk_url =
-  "https://odk.gramvaani.org/-/single/XbJ9whVVE7tLdGeu2he992qvYcjNx32?st=JX7YnWnvP6iqw43yTCCrEgn6rq38RJT3mE7o7HCfC4Q6uJVWSAqSBaeoJFKlVBWf";
+  "https://odk.core-stack.org/-/single/veabb7xog6Xh5THL6DQoSspRufghwX9?st=2sWYsnvVeFdkr12RHgZA6IzyYV1cIqpY5n4thlMP3AxQ0vS!ilXolTVwGSy7Eo9P";
 
+// settlement form
 exports.add_hm_odk_url =
-  "https://odk.gramvaani.org/-/single/iBOYbTqlqPwsDxkgG5UZ49MQHrrpGAZ?st=ggKVlBbRRKdYZLR2mGVxyItq0aRAUcRKTfikbwZUNyWSyPM$akCxgcaiZh9Qvlpk&d[/data/GPS_point/point_mapsappearance]=";
+  "https://odk.core-stack.org/-/single/cQZJMHNXzplpHgcMV3cGsNOxZ8esEwt?st=TBomGhMfOetjH6thCwy$zzXyj!5bZs6Q20MejPCDCdNmX7IO9MqzRB6DkJ$PEOpl&d[/data/GPS_point/point_mapsappearance]=";
+// well form
 exports.add_well_odk_url =
-  "https://odk.gramvaani.org/-/single/MbASrc3QO18p7P0nmvIwAMgnQm76LBR?st=7nfZB8pSIneSqhoDUIwelgux4QUmVW3xaNBEy2ONMan6h5YcuyZhBrxj6s0uhUis&d[/data/GPS_point/point_mapappearance]=";
+  "https://odk.core-stack.org/-/single/lXPP1LHKFKsyMNGljtjsjcgYgZgOtug?st=19BTDQ31numlgTB5vTjZZZxrgPRtYv1TZtNw2662Md$OA0uqHioc3xYYZ8lNwrar&d[/data/GPS_point/point_mapappearance]=";
+// recharge structure
 exports.add_pr_new_rc_st_odk_url =
-  "https://odk.gramvaani.org/-/single/SJfpkTOoBwx71amP1AUM1OGd8h72Ej4?st=DIovSA!ytbl6ixGi6jspvWUHFWj$kleMu$UIs3176kF$8xt2PGgIMSDp5hDIg6Db&d[/data/GPS_point/point_mapsappearance]=";
+  "https://odk.core-stack.org/-/single/97dnXFfIiBSX8NflbKwiJeuo6jAkeji?st=dzcP1R16z3P$seU5QjKkgGjdav$pga66iH972V93scb8BxwvuBuQVxIUxLo$gf8F&d[/data/GPS_point/point_mapsappearance]=";
+
+// TODO: add the new link
+// maintenance remote swb
 exports.add_propose_maintainence_satellite_odk_url =
   "https://odk.gramvaani.org/-/single/7sf9nT6OBSpm2IrfpFvzwpiRoJ54JBa?st=E601NPG8bJwF8uoGXVmWtH2Q7aFZJo2Wzkg$zsF8xTTrt3ONGuK6pvM4P!cpQLoo&d[/data/GPS_point/point_mapsappearance]=";
+// maintenance swb
 exports.add_remote_sensed_waterbodies_odk_url =
-  "https://odk.gramvaani.org/-/single/IlhFTDvrF1Sib1NxpJrybXIfko9ZD80?st=sR!eg7pgy$DCm66Dw614v!PDHuFXFG61SyYo9mUvjb5l0IOTouwflpQvR0oxoJaX&d[/data/GPS_point/point_mapsappearance]=";
+  "https://odk.core-stack.org/-/single/6LuxBpV2ohTtxJSnFcwhGS399vAJwe0?st=jCR5wJ!ZjU5!yrQt0e7KoV9eOmvHgKiLwRIiL40IuoZ53yQ8VNH4yb!N7k0Of7Tp&d[/data/GPS_point/point_mapsappearance]=";
 exports.add_new_ir_st_odk_url =
-  "https://odk.gramvaani.org/-/single/XbJ9whVVE7tLdGeu2he992qvYcjNx32?st=JX7YnWnvP6iqw43yTCCrEgn6rq38RJT3mE7o7HCfC4Q6uJVWSAqSBaeoJFKlVBWf&d[/data/GPS_point_irrigation_work/point_mapsappearance]=";
-exports.add_wb_odk_url =
-  "https://odk.gramvaani.org/-/single/Jlj1CgdqEkuE6mdcdsygicWMyhFzel2?st=FyjvdtREP5JYwGD4o5t1jugWf4nRlBkq2MpwSKRAEJqRK0o0yMTjytzw7gUTE8SK&d[/data/GPS_point/point_mapappearance]=";
-exports.add_livelihood_odk_url =
-  "https://odk.gramvaani.org/-/single/kmd5gEiN0Jj4nQ3QXhvgyStCI7tBu8g?st=Rwcz9GqN4CcWonNuQElcbsHFnbDy1Tgl3aCsIJLvdgsuI7pF$j31oQqcbrDV5Z2u&d[/data/GPS_point/point_mapappearance]=";
+  "https://odk.core-stack.org/-/single/veabb7xog6Xh5THL6DQoSspRufghwX9?st=2sWYsnvVeFdkr12RHgZA6IzyYV1cIqpY5n4thlMP3AxQ0vS!ilXolTVwGSy7Eo9P&d[/data/GPS_point_irrigation_work/point_mapsappearance]=";
 
-exports.confirm_well_odk_url =
-  "https://odk.gramvaani.org/-/single/8bcQW97e84w4VKT5OlceYpyZnHOPe2D?st=S7aarupn2MyT$wDZpkF4J2MHR1iv8RK3suxqgdGROnoVoj!EXGV8ZMHZ6BwI4rOJ&d[/data/Hamlet_id]=";
+// water structure form
+exports.add_wb_odk_url =
+  "https://odk.core-stack.org/-/single/V6wADZAa7gqL6qFi5U35TrdmOMWIOaC?st=XidlpvnHZfCMxcDlxXr4Jbxr6eKwtsclHZki7s9pbiIb14rPh7uNYCalXHs0M6ht&d[/data/GPS_point/point_mapappearance]=";
+// livelihood
+exports.add_livelihood_odk_url =
+  "https://odk.core-stack.org/-/single/36yDHyNVwmE3y7tLY9eLRdcy5QQXJuZ?st=1PZrxqQxTnyseflapUolLDE6a5U1rw98FDgTuiORL7Of532h3zsnV!fnhA2ZbL1w&d[/data/GPS_point/point_mapappearance]=";
+
+// exports.confirm_well_odk_url =
+//   "https://odk.gramvaani.org/-/single/8bcQW97e84w4VKT5OlceYpyZnHOPe2D?st=S7aarupn2MyT$wDZpkF4J2MHR1iv8RK3suxqgdGROnoVoj!EXGV8ZMHZ6BwI4rOJ&d[/data/Hamlet_id]=";
+
+// crop info form
 exports.add_crop_info_odk_url =
-  "https://odk.gramvaani.org/-/single/HoKR7LdbXlUL9eqYFCS5cJcH8cAq9ls?st=nt1u0UnOF9YWe6v$j6dJZGHNev7paLOFlUDpK4Mil7BAeL9H5pTCLFKCjfC6tuS4&d[/data/hamlet_id]=";
-exports.provide_info_odk_url =
-  "https://odk.gramvaani.org/-/single/7ElY7YwrD3srcL289f9bCtXrfFsk6Km?st=lNG74u1Bez32eeZE7s0RMDT6BcPJWvvlLI66exi4upkBF40KqMiu1dSSayifrIA9&d[/data/hamlet_id]=";
-exports.confirm_crop_odk_url =
-  "https://odk.gramvaani.org/-/single/4gaGLhGgoQMmeg58xPc99JiZLXK7Tso?st=!s7R5zFi$HimWa7Koa8kWlWS64e!zqNcOKLd06sdx1V6X2YONojhSs2JzJWgFTpM&d[/data/Hamlet_id]=";
-exports.map_hamlet_agri_odk_url =
-  "https://odk.gramvaani.org/-/single/NqFXYqHg6ZzUAsA3gaBWnngxmZp3Q7y?st=atZnDr0mwkJY$eATrIRD!8oPWnpsRS$CcheJpMZVT2yrcIeQP7Qap2nqYjt$NQtW&d[/data/hamlet_id]=";
-exports.map_hamlet_gw_odk_url =
-  "https://odk.gramvaani.org/-/single/y9pZOV6xtAVWoaY3gDlKhXDcxRnLrn2?st=NX1noPn21kBb6hIAWB7PKUyN26!WwiSMb8plxNqa!YD8C4iTJbJR5p25LIl2zZB8&d[/data/hamlet_id]=";
-exports.map_hamlet_wb_odk_url =
-  "https://odk.gramvaani.org/-/single/jbKD1Vn4RPf1UUTfbn7MYufROkbKuR5?st=8Z9Z6Q6lzcTUi7TZvZe5ntDZW9vbzpuf8xdgyA8LETWYNcMqBIvTUyDql4jV4kDf&d[/data/hamlet_id]=";
+  "https://odk.core-stack.org/-/single/pzVQuaWY2OhMX8PKoV35pcUWWgCKNLD?st=z44571PIPvbF2lpzcaRZruzCVIucT8Agm!ZfbrhUxUwhY6l78V8Vs5P!$smauFD3&d[/data/hamlet_id]=";
+// exports.provide_info_odk_url =
+//   "https://odk.gramvaani.org/-/single/7ElY7YwrD3srcL289f9bCtXrfFsk6Km?st=lNG74u1Bez32eeZE7s0RMDT6BcPJWvvlLI66exi4upkBF40KqMiu1dSSayifrIA9&d[/data/hamlet_id]=";
+// exports.confirm_crop_odk_url =
+//   "https://odk.gramvaani.org/-/single/4gaGLhGgoQMmeg58xPc99JiZLXK7Tso?st=!s7R5zFi$HimWa7Koa8kWlWS64e!zqNcOKLd06sdx1V6X2YONojhSs2JzJWgFTpM&d[/data/Hamlet_id]=";
+// exports.map_hamlet_agri_odk_url =
+//   "https://odk.gramvaani.org/-/single/NqFXYqHg6ZzUAsA3gaBWnngxmZp3Q7y?st=atZnDr0mwkJY$eATrIRD!8oPWnpsRS$CcheJpMZVT2yrcIeQP7Qap2nqYjt$NQtW&d[/data/hamlet_id]=";
+// exports.map_hamlet_gw_odk_url =
+//   "https://odk.gramvaani.org/-/single/y9pZOV6xtAVWoaY3gDlKhXDcxRnLrn2?st=NX1noPn21kBb6hIAWB7PKUyN26!WwiSMb8plxNqa!YD8C4iTJbJR5p25LIl2zZB8&d[/data/hamlet_id]=";
+// exports.map_hamlet_wb_odk_url =
+//   "https://odk.gramvaani.org/-/single/jbKD1Vn4RPf1UUTfbn7MYufROkbKuR5?st=8Z9Z6Q6lzcTUi7TZvZe5ntDZW9vbzpuf8xdgyA8LETWYNcMqBIvTUyDql4jV4kDf&d[/data/hamlet_id]=";
 exports.feedback_agri_odk_url =
-  "https://odk.gramvaani.org/-/single/E9hUlb5xQiBih7TSmoM6VTDgwtuj1Wv?st=l6kvbwaNkcaIl6inOnPHlZIzQHJBVVvYwJdkzdVsZm!fRf5JqeZbwc70$rLt4Tpa";
+  "https://odk.core-stack.org/-/single/wXLfsaCtKuT8FbN4bAcdQOYG20fszJQ?st=4CQ8YLJgpsTpICSK8P!yv4Z$yAwLAzsh9W$5pkFSxzKWIEDJ37IZHBv7DV2E8SXX";
 exports.feedback_wb_odk_url =
-  "https://odk.gramvaani.org/-/single/Nw3vckrdy9cLPUFdJg2kKpnZkD5TDNg?st=F1cxU!9w!HDYaC7j$nX0icPNklAWL6y$Rd5Tnu8eGrThH!KT1uIxY$dLkFvci$!G";
+  "https://odk.core-stack.org/-/single/ZnvkXctvaccKHTAXU3GwElOQKjHSGjn?st=BwL42ECSlE$DnSOQUBn4156isxk2IyJ2WKRz9W8PS!IfiNp4pSFFSpF3qBITGpfY";
 exports.feedback_gw_odk_url =
-  "https://odk.gramvaani.org/-/single/ijZglMKdfodn80JSFMkdahGrcqxMSbD?st=UpFudgKWgouwuB8KiZnXc01qv790tQf8udPJOb4EykRGsY930w!tiR8k1Psd9E26";
-exports.confirm_wb_odk_url =
-  "https://odk.gramvaani.org/-/single/oV5NrLiqC7SHtYa7XocrqxnVO2LnroE?st=JrpWs4bLHoaeZ8KtmLrsVE7qqZEgz2CRh5RDcKGRMkH6$HOdSZF634Jnu5V7bhfU&d[/data/hamlet_id]=";
+  "https://odk.core-stack.org/-/single/4vp9J6yywY8uRiGchF1krVke1ZVdU5h?st=ON$m7z2cCyJYE5bhwFHWa4$vJNmVNQ7$YpHkMUqCyM!8YC0$GIJGQYnH6nWrFQ1k";
+// exports.confirm_wb_odk_url =
+//   "https://odk.gramvaani.org/-/single/oV5NrLiqC7SHtYa7XocrqxnVO2LnroE?st=JrpWs4bLHoaeZ8KtmLrsVE7qqZEgz2CRh5RDcKGRMkH6$HOdSZF634Jnu5V7bhfU&d[/data/hamlet_id]=";
 
 exports.propose_maintenance_agri_irrigation_odk_url =
-  "https://odk.gramvaani.org/-/single/ADGRqjP1zOAaXfJGiq9QckICyWInuKI?st=6F03M6QbM8X7rdHwx7zjmw!wmHifVqrDkEubS3fTWnI9eWW7VncT$Wxl01nY6n39";
+  "https://odk.core-stack.org/-/single/JLSDm5pF2IkJNbQrWFWeyIRlZXab72I?st=Uffu7PhVnyd0vmikK64kDVZEpS7cV41!ymiq2lIrkWSKJTSUeQHwTyUP6mhn2typ";
 exports.propose_maintenance_groundwater_odk_url =
-  "https://odk.gramvaani.org/-/single/BUbULubEsqhCvtOoRbsI3jwBaqyPlap?st=Q3yOjecjLjYlFlY48bRpt7PBBzATlddcl8vu3bLpYfFy5dJuogyWVsdidIC6r6M4&d[/data/GPS_point/point_mapsappearance]=";
+  "https://odk.core-stack.org/-/single/hlyElktl4iiP20ug67jksVhyfoUuiAe?st=HC0EoYBiODabcQ3y6GJQ5EnjsMtKbiwJWA2ZRSptVG!C0PgMUX6wu4N29kYp9NF8&d[/data/GPS_point/point_mapsappearance]=";
 
 exports.allowedColumns_nrega = [
   "State",
